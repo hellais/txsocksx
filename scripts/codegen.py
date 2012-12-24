29c5ca2f4dc4513566f4aedce865cb69879b001b
Arturo Filastò <art@fuffa.org>
2012-12-24 03:29:09 +0100

    Expand code generator to generate error handling code.

--- scripts/codegen.py
+++ scripts/codegen.py
@@ -18,4 +18,29 @@ def makeTestFailure():
     """
     for i, v in enumerate(l):
         print base % {'idx': i+1, 'error_name': v}
-makeTestFailure()
+
+def errorTrappingFunctions():
+    import re
+    def convert(name):
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+    l = ['ServerFailure',
+         'ConnectionNotAllowed',
+         'NetworkUnreachable',
+         'HostUnreachable',
+         'ConnectionRefused',
+         'TTLExpired',
+         'CommandNotSupported',
+         'AddressNotSupported']
+
+    base = """
+    elif isinstance(failure.value, %(error_name)s):
+        log.err("SOCKS error: %(error_name)s")
+        string = 'socks_%(error_string)s'
+    """
+    for i, v in enumerate(l):
+        error_string = convert(v)
+        print base % {'error_name': v, 'error_string': error_string}
+
+errorTrappingFunctions()
